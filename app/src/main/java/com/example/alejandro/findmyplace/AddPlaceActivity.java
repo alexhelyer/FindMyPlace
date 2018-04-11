d6ac20ce3a1ac4899fb52a594ea42c3de5ce86c2
Alejandro <alexhelyer7@gmail.com>
2018-04-10 20:00:39 -0500

    comentario de alex

--- app/src/main/java/com/example/alejandro/findmyplace/AddPlaceActivity.java
+++ app/src/main/java/com/example/alejandro/findmyplace/AddPlaceActivity.java
@@ -9,5 +9,7 @@ public class AddPlaceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_place);
+
+        //Esto es un comentario de Alex
     }
 }
