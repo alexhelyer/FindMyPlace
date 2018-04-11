af20cd0f8c1c4324070e76b5ff61671fe9ba0277
Alejandro <alexhelyer7@gmail.com>
2018-04-10 19:30:11 -0500

    Creación de actividades

--- app/src/main/java/com/example/alejandro/findmyplace/MapsActivity.java
+++ app/src/main/java/com/example/alejandro/findmyplace/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.alejandro.findmyplace;
 
+import android.content.Intent;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -10,9 +17,10 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback ,View.OnClickListener{
 
     private GoogleMap mMap;
+    private FloatingActionButton addPlaceButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,8 +30,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        addPlaceButton = findViewById(R.id.main_float_button);
+        addPlaceButton.setOnClickListener(this);
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case R.id.saved_places:
+                Intent intent = new Intent(this,SavedPlacesActivity.class);
+                startActivity(intent);
+                break;
+        }
+        return true;
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.main_float_button:
+                Intent intent = new Intent(this,AddPlaceActivity.class);
+                startActivity(intent);
+                break;
+        }
+    }
 
     /**
      * Manipulates the map once available.
