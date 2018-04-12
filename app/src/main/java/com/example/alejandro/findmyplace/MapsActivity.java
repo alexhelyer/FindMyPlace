14db91aca973a6686b8bb30dc40a961d0e9d02a1
Alejandro <alexhelyer7@gmail.com>
2018-04-11 20:04:22 -0500

    no message

--- app/src/main/java/com/example/alejandro/findmyplace/MapsActivity.java
+++ app/src/main/java/com/example/alejandro/findmyplace/MapsActivity.java
@@ -1,7 +1,14 @@
 package com.example.alejandro.findmyplace;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,6 +16,7 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toast;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -17,11 +25,13 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback ,View.OnClickListener{
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback, View.OnClickListener {
 
     private GoogleMap mMap;
     private FloatingActionButton addPlaceButton;
+    private Place place;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,8 +40,30 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        requestPermissions(new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION}, 43);
         addPlaceButton = findViewById(R.id.main_float_button);
         addPlaceButton.setOnClickListener(this);
+
+        place = new Place();
+        //place.setLocation(new LatLng(19.102933,-99.193289));
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        Localizacion localizacion = new Localizacion(place);
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return;
+        }
+
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER,0, 0, localizacion);
+
     }
 
     @Override
@@ -56,8 +88,15 @@ public class MapsActivity extends AppCompatActivity implements OnMapReadyCallbac
     public void onClick(View view) {
         switch (view.getId()){
             case R.id.main_float_button:
-                Intent intent = new Intent(this,AddPlaceActivity.class);
-                startActivity(intent);
+
+                if (place.getLocation()!=null) {
+                    Intent intent = new Intent(this,AddPlaceActivity.class);
+                    intent.putExtra("LATITUDE", place.getLocation().latitude);
+                    intent.putExtra("LONGITUDE", place.getLocation().longitude);
+                    startActivity(intent);
+                } else {
+                    Toast.makeText(this, "Aun no se ha detectado el GPS", Toast.LENGTH_SHORT).show();
+                }
                 break;
         }
     }
