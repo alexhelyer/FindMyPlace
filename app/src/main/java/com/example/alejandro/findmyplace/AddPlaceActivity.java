e3e1f717ce3b1b3ee81a099f4ab1e86e25161cfb
Diego Montero <diego.ams.1893@gmail.com>
2018-04-15 14:12:50 -0500

    Solicitud de permisos para almacenamiento externo

--- app/src/main/java/com/example/alejandro/findmyplace/AddPlaceActivity.java
+++ app/src/main/java/com/example/alejandro/findmyplace/AddPlaceActivity.java
@@ -1,9 +1,14 @@
 package com.example.alejandro.findmyplace;
 
+import android.Manifest;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Color;
 import android.net.Uri;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -22,6 +27,7 @@ import com.google.android.gms.maps.model.LatLng;
 
 public class AddPlaceActivity extends AppCompatActivity {
 
+    private static final int MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE = 100;
     //Views
     ImageView category;
     EditText title;
@@ -69,9 +75,7 @@ public class AddPlaceActivity extends AppCompatActivity {
         photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
-                photoPickerIntent.setType("image/*");
-                startActivityForResult(photoPickerIntent, 100);
+               checkPermissionForPicture();
             }
         });
 
@@ -281,7 +285,38 @@ public class AddPlaceActivity extends AppCompatActivity {
         });
 
 
+    }
 
+    private void checkPermissionForPicture(){
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE);
 
+        } else {
+            getPictureFromGallery();
+        }
+    }
+
+    private void getPictureFromGallery(){
+        Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
+        photoPickerIntent.setType("image/*");
+        startActivityForResult(photoPickerIntent, 100);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_EXTERNAL_STORAGE:
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    getPictureFromGallery();
+
+                } else {
+                    Toast.makeText(this, getString(R.string.grant_external), Toast.LENGTH_SHORT).show();
+                }
+                break;
+
+        }
     }
 }
